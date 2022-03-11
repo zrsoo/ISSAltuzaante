04dec8bd4012cfba7e8c9c03b34bf17e3f05905e
915-Murariu-Alexandra <72067830+915-Murariu-Alexandra@users.noreply.github.com>
2022-03-11 21:47:28 +0200

    added template MVC .NET Core Web App with dedicated gitignore

---
+++ AcademicInfo/AcademicInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcademicInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
