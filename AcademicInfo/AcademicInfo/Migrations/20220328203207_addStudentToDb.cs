5d2f49f74cb56a9b1b9bd85104d20b190048a8b1
915-Murariu-Alexandra <72067830+915-Murariu-Alexandra@users.noreply.github.com>
2022-03-28 23:40:33 +0300

    added student table to DB

---
+++ AcademicInfo/AcademicInfo/Migrations/20220328203207_addStudentToDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicInfo.Migrations
+{
+    public partial class addStudentToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- AcademicInfo/AcademicInfo/Program.cs
+++ AcademicInfo/AcademicInfo/Program.cs
@@ -1,4 +1,6 @@
 using AcademicInfo.Config;
+using AcademicInfo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +11,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//builder.Services.AddIdentity<User, IdentityRole>()
-//    .AddEntityFrameworkStores<ApplicationDbContext>()
-//    .AddDefaultTokenProviders();
+builder.Services.AddIdentity<Student, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 
 builder.Services.AddCors(options =>
