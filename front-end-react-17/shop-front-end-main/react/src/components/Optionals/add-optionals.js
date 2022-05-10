5e5b14b2c0ff198bffc1108d790f1f9b0cc52a25
915-Neamtiu-Zaris <46485193+zrsoo@users.noreply.github.com>
2022-05-10 10:55:12 +0300

    asd

---
+++ front-end-react-17/shop-front-end-main/react/src/components/Optionals/add-optionals.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Optionals/addOptionals.css"
+import { useState } from 'react'
+
+export default function AddOptionals(props){
+    return (
+        <div className="optionals-wrapper">
+            <div className='optionals-info-wrapper'>
+                
+            </div>
+            <div className="optionals-form-wrapper">
+                
+            </div>
+        </div>   
+    );
+}
