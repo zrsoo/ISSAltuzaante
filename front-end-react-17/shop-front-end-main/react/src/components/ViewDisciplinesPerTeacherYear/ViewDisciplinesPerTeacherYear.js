d0c5d20ab2b1ad55bea1642ce8d5d3fa789ccc86
915-Neamtiu-Zaris <46485193+zrsoo@users.noreply.github.com>
2022-05-21 17:43:29 +0300

    placed dropdowns for teacher and year; installed yarn; used yarn to build dependecies

--- front-end-react-17/shop-front-end-main/react/src/components/ViewDisciplinesPerTeacherYear/ViewDisciplinesPerTeacherYear.js
+++ front-end-react-17/shop-front-end-main/react/src/components/ViewDisciplinesPerTeacherYear/ViewDisciplinesPerTeacherYear.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from 'react'; 
 import { useState, useEffect } from 'react';
 import DisciplineController from '../../controllers/DisciplineController';
 import "../ViewDisciplinesPerTeacherYear/ViewDisciplinesPerTeacherYear.css";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import Dropdown  from 'react-bootstrap/Dropdown';
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 
 export default function ViewDisciplinesPerTeacherYear() {
     const [optionals, setOptionals] = useState([]);
@@ -33,27 +38,48 @@ export default function ViewDisciplinesPerTeacherYear() {
         }
     }
 
+    //  TODO:    1.) Make a function similar to renderTableData that loads Drowpdown.Items in dropdowns (containing all teacher emails, it's ok to hardcode years)
+    //           2.) For each click on an option, perform GET request (most comfortably with query parameters in link) that gets disciplines for specified teacher and year
+    //           3.) Once a teacher or a year is selected, set the Dropdown.Toggle to display the selected item.
+     
     return (
-        <div className="contentWrapper">
-            <div className="discipline-table-wrapper">
-                <table className='table table-striped'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>IsOptional</th>
-                            <th>FacultyId</th>
-                            <th>Number of students</th>
-                            <th>Maximum number of students</th>
-                            <th>Year</th>
-                            <th>Teacher Email</th>
-                            <th>Average grade</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {renderTableData()}
-                    </tbody>
-                </table>
-            </div>
+      <div className="contentWrapper">
+        <div className="dropdownContainer">
+          <Dropdown className='teacherDropdown'>
+            <Dropdown.Toggle variant="success">Teacher email</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#">Home Page</Dropdown.Item>
+              <Dropdown.Item href="#">Settings</Dropdown.Item>
+              <Dropdown.Item href="#">Logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Dropdown className='yearDropdown'>
+            <Dropdown.Toggle variant="success">Year</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#">Home Page</Dropdown.Item>
+              <Dropdown.Item href="#">Settings</Dropdown.Item>
+              <Dropdown.Item href="#">Logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
+        <div className="discipline-table-wrapper">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>IsOptional</th>
+                <th>FacultyId</th>
+                <th>Number of students</th>
+                <th>Maximum number of students</th>
+                <th>Year</th>
+                <th>Teacher Email</th>
+                <th>Average grade</th>
+              </tr>
+            </thead>
+            <tbody>{renderTableData()}</tbody>
+          </table>
+        </div>
+      </div>
     );
 }
